34f4398093447b92bbf90a223a2390173ff34adb
Kingwizard96 <calvarado1996@gmail.com>
2023-10-16 02:36:20 -0500

    didnt lose any data when low battery shutdown happend

--- main/db/schema.sql
+++ main/db/schema.sql
@@ -3,4 +3,15 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    role_title VARCHAR(30) NOT NULL,
+    role_salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,    
+);
