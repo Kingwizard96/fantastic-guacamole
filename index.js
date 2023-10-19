ab7a2b61813a8a2463f727a1ebea1aac47d61f84
Kingwizard96 <calvarado1996@gmail.com>
2023-10-19 05:59:53 -0500

    up and running,low energy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function init() {
         type: 'list',
         name: 'choice',
         message: 'What would you like to do?',
-        choices: ["View Departments", "View Roles", "View Employees"]
+        choices: ["View Departments", "View Roles", "View Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role", "Exit"]
     },
     ]).then((answer) => {
         console.log(answer);
@@ -17,8 +17,25 @@ function init() {
         if(answer.choice === "View Departments") {
             viewDepartments();
             // write  the logic to view departments
+        }else if(answer.choice === "View Roles") {
+            viewRoles();
+        }else if(answer.choice === "View Employees") {
+            viewEmployees();
+        }else if(answer.choice === "Add Department") {
+            addDepartment();
+        }else if(answer.choice === "Add Role") {
+            addRole();
+        }else if(answer.choice === "Add Employee") {
+            addEmployee();
+        }else if(answer.choice === "Update Employee Role") {
+            updateEmployeeRole();
+        }else {
+            console.log("Goodbye!");
+            process.exit();
         }
+            // write the logic to view roles
     });
+
 };
 
     function viewDepartments() {
@@ -41,12 +58,94 @@ function init() {
 
     function viewEmployees() {
         console.log("Viewing Employees...");
-        db.promise().query('SELECT * FROM employee').then(([rows]) => { 
+        db.promise().query('SELECT * FROM employees').then(([rows]) => { 
             console.table(rows);
         }).then(() => {
             init();
         });
     };
+
+    function addDepartment() {
+        console.log("Adding Department...");
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'department_name',
+                message: 'What is the name of the department you would like to add?',
+            },
+        ]).then((answer) => {
+            console.log(answer);
+            db.promise().query('INSERT INTO department SET ?', answer).then(([rows]) => {
+                console.table(rows);
+                init();
+            });
+        });
+    };
+
+    function addEmployee() {    
+       console.log("Adding Employee...");
+       inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is the first name of the employee you would like to add?',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the employee first name!');
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the last name of the employee you would like to add?',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the employee last name!');
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'What is the role id of the employee you would like to add?',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the employee role id!');
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'What is the manager id of the employee you would like to add?',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the employee manager id!');
+                  return false;
+                }
+              }
+        },
+       ]).then(answer => {
+        console.log(answer);
+        db.promise().query('INSERT INTO employees SET ?', answer).then(([rows]) => {
+            console.table(rows);
+            init();
+        });
+       });    
+    };
+    
 
 
 init();
