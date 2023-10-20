a4f41b1e7690dcb2cc36f8418b9ba9d646e8c67c
Kingwizard96 <calvarado1996@gmail.com>
2023-10-19 19:06:19 -0500

    init commit commit innit

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
         //  MySQL username,
         user:'root',
         //  MySQL password (error will happen if you don't have a password)
-        // password: '2442@Wizard!',
+        password: '2442@Wizard!',
         database: 'tracker_db'
     },
     console.log(`Connected to the tracker_db database.`)
